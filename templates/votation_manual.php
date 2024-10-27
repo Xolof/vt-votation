dbf66c685a848af900d4cf3567d49ec59c00c89f
Olof <olof@liberdev.se>
2024-10-27 14:41:13 +0700

    Update manual

--- templates/votation_manual.php
+++ templates/votation_manual.php
@@ -5,12 +5,10 @@ if (!defined('ABSPATH')) {
 ?>
 
 <h1>Manual</h1>
-<h3>Syfte</h3>
-<p>Tanken med pluginet är att förhindra spam och sammanställa resultaten från omröstningen.</p>
 <h3>Funktionalitet</h3>
 <ul>
   <li>Sammanställer resultat från omröstning och visar upp i admin.</li>
-  <li>Tar endast emot en röst per epostadress.</li>
-  <li>Gör det möjligt att endast tillåta en inlämning per IP-adress.</li>
+  <li>Tar endast emot en röst per bok per epostadress.</li>
+  <li>Gör det möjligt att endast tillåta en röst per bok per IP-adress.</li>
   <li>Möjliggör blockering av IP-adresser.</li>
 </ul>
